f8ea31a2db9314c94fdd72850038e7d0d8e64b85
Chaigood <cj.haigood@gmail.com>
2020-11-03 12:48:43 -0600

    CSS add-on

--- script.js
+++ script.js
@@ -10,7 +10,43 @@ h2E2.textContent = "Javascript is hard. This timed quiz will test your skills. B
 body.appendChild(h2E2).style.textAlign = "center";
 body.appendChild(h2E2).style.color = "blue";
 
-function button() {
-    var btn = document.createElement("Button");
-    btn.innerHTML = "Wrong";
+var carousel = document.querySelector(".carouselbox");
+var next = carousel.querySelector(".next");
+var prev = carousel.querySelector(".prev");
+carousel.style.backgroundImage = "url('https://picsum.photos/300/200')";
+var index = 0;
+var images = [
+  "https://picsum.photos/300/200",
+  "https://picsum.photos/300/201",
+  "https://picsum.photos/300/202",
+  "https://picsum.photos/300/203"
+];
+var currentImage;
+function navigate(direction) {
+  index = index + direction;
+  if (index < 0) { 
+    index = images.length - 1; 
+  } else if (index > images.length - 1) { 
+    index = 0;
+  }
+  currentImage = images[index];
+  carousel.style.backgroundImage = "url('" + currentImage + "')";
 }
+
+carousel.addEventListener("click", function() {
+  window.location.href = images[index];
+});
+
+next.onclick= function(event) {
+  event.stopPropagation();
+
+  navigate(1);
+};
+
+prev.addEventListener("click", function(event) {
+  event.stopPropagation();
+
+  navigate(-1);
+});
+
+navigate(0);
